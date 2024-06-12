fbc3179f1a2d7c30efde17fd566c1b9ee82c1e55
DESKTOP-D2UBERD\coms <https://github.com/TalpangEE>
2024-06-12 20:31:50 +0900

    오타수정

--- src/handlers/item.handler.js
+++ src/handlers/item.handler.js
@@ -4,7 +4,7 @@ import { getStage, setStage } from '../models/stage.model.js';
 
 // 아이템 획득 핸들러
 export const itemGetHandler = (userId, payload) => {
-  const { itemId, timestamp } = payload;
+  const { id, timestamp } = payload;
   const { items, itemUnlocks } = getGameAssets();
 
   // 유저의 현재 스테이지 정보 가져오기
@@ -12,14 +12,14 @@ export const itemGetHandler = (userId, payload) => {
   const currentStage = userStage[userStage.length - 1].id;
 
   // 아이템 존재 여부 확인 및 점수 가져오기
-  const item = items.data.find((i) => i.id === itemId);
+  const item = items.data.find((i) => i.id === id);
   if (!item) {
     return { status: 'fail', message: '아이템이 존재하지 않음' };
   }
 
   // 아이템이 현재 스테이지에서 획득 가능한지 확인
   const stageItemUnlock = itemUnlocks.data.find(
-    (info) => info.stage_id === currentStage && info.item_id === itemId,
+    (info) => info.stage_id === currentStage && info.item_id === id,
   );
   if (!stageItemUnlock) {
     return { status: 'fail', message: '획득할 수 없는 아이템' };
